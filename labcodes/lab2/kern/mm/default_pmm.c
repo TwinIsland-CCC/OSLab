52806763a2ff4816355060cea6722ec31924e3fa
TwinIsland-CCC <593312487@qq.com>
2022-11-14 20:58:48 +0800

    1

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -174,7 +174,6 @@ default_free_pages(struct Page *base, size_t n) {
             p->property += base->property;
             ClearPageProperty(base);
             base = p;
-            base = p;;;
             list_del(&(p->page_link));
         }
     }
